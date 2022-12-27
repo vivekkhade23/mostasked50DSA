301b270e99700ca8f6d6012bfbf165bd6fdd37a0
vivekkhade23 <khadev881@gmail.com>
2022-12-27 19:15:05 +0530

    two sum

---
+++ twossum-sortedarray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let l=0;
+    let r=numbers.length-1;
+    while(l<r){
+        let sum=numbers[l]+numbers[r];
+        if(sum==target){
+            return [l+1,r+1];
+        }
+        else if(sum>target){
+            r--;
+        }
+        else{
+            l++;
+        }
+    }
+};
